9342d5ee6bdb32e89fbc2d19b9c8a1afce9cac2a
Isaiah <ijandicos90@gmail.com>
2022-08-01 09:33:25 -0400

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # Add a console message so we can see output when the seed file runs
+50.times do
+    Game.create(
+title: Faker::Game.title,
+genre: Faker::Game.genre,
+platform: Faker::Game.platform,
+price: rand(0..60) # random number between 0 and 60 
+    ) 
+end
+
+puts "Done seeding!"
